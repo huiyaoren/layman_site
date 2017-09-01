005e91d9b610827c4e606d46438c7c48f6d46915
wslshanlin@sina.com <wslshanlin@sina.com>
2017-09-01 21:57:52 +0800

    newFeature | 场景面向对象

--- app/static/js/game/scene.js
+++ app/static/js/game/scene.js
@@ -19,15 +19,3 @@ class BaseScene {
     }
 }
 
-class SceneTitle extends BaseScene {
-    constructor(game) {
-        super(game);
-        game.registerAction('k', function () {
-            var s = Scene(game);
-            game.replaceScene(s);
-        });
-    }
-    draw() {
-        this.game.context.fillText('按 k 开始游戏', 100, 200);
-    }
-}
--- app/static/js/scene/title/scene_title.js
+++ app/static/js/scene/title/scene_title.js
@@ -1,21 +1,12 @@
-var SceneTitle = function (game) {
-    var s = {
-        game: game,
-    };
-
-    game.registerAction('k', function () {
-         var s = Scene(game);
-        game.replaceScene(s);
-    });
-
-    s.draw = function () {
-
-        game.context.fillText('按 k 开始游戏', 100, 200);
-    };
-
-    s.update = function () {
-
-    };
-
-    return s;
-};
+class SceneTitle extends BaseScene {
+    constructor(game) {
+        super(game);
+        game.registerAction('k', function () {
+            var s = Scene(game);
+            game.replaceScene(s);
+        });
+    }
+    draw() {
+        this.game.context.fillText('按 k 开始游戏', 100, 200);
+    }
+}
