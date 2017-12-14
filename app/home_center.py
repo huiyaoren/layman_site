6bd16e24471464f2b35bad7dfbd7e4bd54685b75
huiyaoren <wslshanlin@sina.com>
2017-12-14 17:56:12 +0800

    newFeature | 新增黄金价格 天气模块

--- app/home_center.py
+++ app/home_center.py
@@ -2,54 +2,90 @@ import requests
 from lxml import etree
 
 
-class Weather():
+class Parser():
     def __init__(self):
-        self.location = 'FuZhou'
-        self.url_weather_day = 'http://tianqi.2345.com/fuzhou/58847.htm'
-        # self.url_weather_day = 'http://www.weather.com.cn/weather1d/101230101.shtml'
-        # self.url_weather_week = 'http://www.weather.com.cn/weather/101230101.shtml'
-        self.now = {
-            'time': '23:05',
-            'temperature': '24.0',
-            'wind': {
-                'direction': '东北风',
-                'level': '2 级'
-            },
-            'humidity': '87%',
-            'air_quality': '87',
-        }
-        self.day = []
-        self.week = []
         self.xml_parser = etree.HTMLParser(encoding="utf-8")
-        self.run()
+        self.set_config()
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    def response_from_url(self, url): 
-        return requests.get(url=url).text
+    def set_config(self):
+        pass
+
+    def response_from_url(self, url):
+        return requests.get(url=url, headers=self.headers).text
 
     def items_in_page(self, page, xpath_pattern):
         html = etree.HTML(page, parser=self.xml_parser)
         result = html.xpath(xpath_pattern)
-        print(result)
+        return result[0] if len(result) == 1 else result
 
-    def run(self):
-        page = self.response_from_url(self.url_weather_day)
-        # print(page)
-        self.now
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[1]/i/text()')
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[2]/i/text()')
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[3]/i/text()')
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[4]/i/text()')
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[5]/i/text()')
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[6]/i/text()')
-        self.items_in_page(page, '//*[@id="weaLiveInfo"]/ul/li[7]/i/text()')
-        self.items_in_page(page, '//*[@id="wrap"]/div[6]/div[2]/div[1]/div[3]/div/a/text()')
-        self.items_in_page(page, '//*[@id="wrap"]/div[6]/div[2]/div[1]/div[3]/div/a/i/text()')
-        self.items_in_page(page, '//*[@id="life_data"]/div[2]/ul/li/div[2]/h4[@class="green"]/text()')
-        self.items_in_page(page, '//*[@id="life_data"]/div[2]/ul/li/div[2]/h4[@class="red"]/text()')
+    def parse(self):
+        patterns = self.patterns
+        page = self.response_from_url(self.url)
+        self.data = {i: self.items_in_page(page, patterns[i]) for i in patterns}
+
+    def  __getattribute__(self, name):
+        if name == 'data':
+            self.parse()
+        return super().__getattribute__(name)
+
+    def __getattr__(self, name):
+        if name == 'data':
+            self.parse()
+            return self.data
+
+    def __getitem__(self, key):
+        if key == 'data':
+            self.parse()
+            return self.data
+
+
+class Weather(Parser):
+    def set_config(self):
+        self.location = 'FuZhou'
+        self.url = 'http://tianqi.2345.com/fuzhou/58847.htm'
+        self.patterns = {
+            'temperature_current': '//*[@id="weaLiveInfo"]/ul/li[1]/i/text()',
+            'humidity': '//*[@id="weaLiveInfo"]/ul/li[2]/i/text()',
+            'wind': '//*[@id="weaLiveInfo"]/ul/li[3]/i/text()',
+            'pressure': '//*[@id="weaLiveInfo"]/ul/li[4]/i/text()',
+            'sunset': '//*[@id="weaLiveInfo"]/ul/li[5]/i/text()',
+            'sunrise': '//*[@id="weaLiveInfo"]/ul/li[6]/i/text()',
+            'uv': '//*[@id="weaLiveInfo"]/ul/li[7]/i/text()',
+            'weather': '//*[@id="wrap"]/div[6]/div[2]/div[1]/div[3]/div/a/text()',
+            'temperature_range': '//*[@id="wrap"]/div[6]/div[2]/div[1]/div[3]/div/a/i/text()',
+        }
+
+
+class Gold(Parser):
+    def set_config(self):
+        self.url = 'http://www.icbc.com.cn/ICBCDynamicSite/Charts/GoldTendencyPicture.aspx'
+        self.patterns = {
+            '人民币账户黄金中间价': '//*[@id="TABLE1"]/tbody/tr[2]/td[5]/text()',
+        }
+
+
+class Bitcoin(Parser):
+    def set_config(self):
+        self.url = 'http://www.bestopview.com/rmb/'
+        self.patterns = {
+            'title': '//title/text()',
+            '美元': '//span/text()',
+        }
+
+    # def parse(self):
+    #     patterns = self.patterns
+    #     page = self.response_from_url(self.url)
+    #     self.data = {1: page}
 
 
 def main():
-    weather = Weather()
+    pass
+
+
+weatherParser = Weather()
+goldParser = Bitcoin()
+
 
 if __name__ == '__main__':
     main()
