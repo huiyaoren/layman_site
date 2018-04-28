7d28fd98b43e05b7e66484a0da2ac1a764c3dfa0
hanlin <hanlin>
2018-04-28 17:08:09 +0800

    newFeature | home_data

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,15 @@
+import json
+
 from flask import render_template
 
 from . import main
 from app.parsers import weatherParser, goldParser, bitcoinParser
 
+
 @main.route('/')
 def index():
     return render_template('index.html')
+
 
 @main.route('/home_center')
 @main.route('/homecenter')
@@ -14,3 +18,16 @@ def home_center():
     gold = goldParser['data']
     bitcoin = bitcoinParser['data']
     return render_template('home_center.html', **locals())
+
+
+@main.route('/home/<name>', methods=['GET'])
+def home_data(name):
+    if name == 'weather':
+        data = weatherParser['data']
+    elif name == 'bitcoin':
+        data = bitcoinParser['data']
+    elif name == 'gold':
+        data = goldParser['data']
+    else:
+        data = {}
+    return json.dumps(data)
