d9eb9f9cc89a9f80718c7abca6a704289f6aeef2
hanlin <zhenghanlin@51zan.com>
2018-08-16 15:02:21 +0800

    结构变更

--- config/__init__.py
+++ config/__init__.py
@@ -1,24 +1,9 @@
 import os
 
-from config.block_chain import BlockChainConfig
+from .block_chain import BlockChainConfig
+from .default import DefaultConfig, Config
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-
-    @staticmethod
-    def init_app(app):
-        pass
-
-
-class DefaultConfig(Config, BlockChainConfig):
-    REDIS_HOST = os.getenv('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')
-    REDIS_PORT = int(os.getenv('REDIS_PORT_6379_TCP_PORT', 6379))
-    REDIS_PASSWORD = os.getenv('REDIS_ENV_REDIS_AUTH')
-    REDIS_DB = int(os.getenv('REDIS_ENV_REDIS_DB', 0))
-
 
 config = {
     'config': Config,
---
+++ config/default.py
@@ -0,0 +1,17 @@
+import os
+
+from config import BlockChainConfig
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DefaultConfig(Config, BlockChainConfig):
+    REDIS_HOST = os.getenv('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1')
+    REDIS_PORT = int(os.getenv('REDIS_PORT_6379_TCP_PORT', 6379))
+    REDIS_PASSWORD = os.getenv('REDIS_ENV_REDIS_AUTH')
+    REDIS_DB = int(os.getenv('REDIS_ENV_REDIS_DB', 0))
