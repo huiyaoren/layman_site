c6ce403347158e5380b79a1b535f13a13820d4e9
wslshablin <wslshanlin@sina.com>
2019-04-21 23:00:03 +0800

    newFeature | my_balance

--- web/src/App.js
+++ web/src/App.js
@@ -5,6 +5,7 @@ import Weather from './components/weather/index';
 import FutureWeather from './components/future_weather/index';
 import Datetime from './components/datetime/index';
 import Gold from './components/gold/index';
+import MyBalance from './components/my_balance/index';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           {/*<FutureWeather/>*/}
           <Datetime/>
           <Gold/>
+          <MyBalance/>
           </>
       );
   }
