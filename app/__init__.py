6ea663773ba0c317971614ad3bdd54839012e9e4
wslshablin <wslshanlin@sina.com>
2019-04-21 18:11:37 +0800

    newFeature | cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
@@ -17,6 +18,7 @@ def create_app(config_name):
 
     db.init_app(app)
     login_manager.init_app(app)
+    CORS(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
