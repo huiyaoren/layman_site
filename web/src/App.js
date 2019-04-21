c8ddbcafa13ab26506c46da89f58b59e19a7be70
wslshablin <wslshanlin@sina.com>
2019-04-21 20:06:28 +0800

    fixFeature | style

--- web/src/App.js
+++ web/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import './Icon.css';
 import Weather from './components/weather/index';
 
 class App extends Component {
   render() {
-    return (
-      <Weather/>
+      return (
+          <>
+              <Weather/>
+          </>
     );
   }
 }
--- web/src/components/weather/index.js
+++ web/src/components/weather/index.js
@@ -69,50 +69,56 @@ class Weather extends Component {
         const weather = data['天气'] && data['天气'].split('转') || ''
 
         return (
-            <>
-            <div id="weather_block" style={{'transition': 'opacity 1s'}}>
-                <table className="layui-table" lay-even="" lay-skin="nob"
-                       style={{'backgroundColor': 'rgba(0,0,0,0)'}}>
-                    <tbody>
-                    <tr>
-                        <td><i className="iconfont table-icon icon-kongqishidu"> </i></td>
-                        <td>[<span id="val_hud" className="table-value">{data['湿度']}</span>]</td>
-                        <td><i className="iconfont table-icon icon-richu"> </i></td>
-                        <td>[<span id="val_sun_on" className="table-value">{data['日出时间']}</span>]</td>
-                        <td><i className="iconfont table-icon icon-riluo"> </i></td>
-                        <td>[<span id="val_sun_off" className="table-value">{data['日落时间']}</span>]</td>
-                        <td><i className="iconfont table-icon icon-daqiyali"> </i></td>
-                        <td>[<span id="val_press" className="table-value">{data['气压']}</span>]</td>
-                        <td><i className="iconfont table-icon icon-ziwaixianzhishu"> </i></td>
-                        <td>[<span id="val_uv" className="table-value">{data['紫外线']}</span>]</td>
-                        <td><i className="wind iconfont table-icon icon-wind-1"> </i></td>
-                        <td>[<span id="val_wind" className="table-value">{data['风级']}</span>]</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div style={{position: 'relative'}}>
-                    <div style={{'width': '17.5rem','position': 'absolute','padding':'0.875rem','left': 0,'top': 0}}>
-                        <i className={`iconfont weather_1 ${weather_list[weather[0]]}`}
-                           style={{'fontSize': '10rem'}}> </i>
-                        {weather.length === 2 ? (
-                            <>
-                            <span className="weather_or" style={{'fontSize': '2rem'}}> </span>
-                            <i className="iconfont weather_2" style={{'fontSize': '5rem'}}> </i>
-                            </>
-                        ) : ''}
-                    </div>
-                    <div >
-                        <span style={{'fontSize': '1.1rem'}} id="val_weather">{data['天气']}</span>
-                        <span style={{'fontSize': '1.1rem'}} id="val_tem_r">{data['气温']}</span>
-                        <br/>
-                        <i className="iconfont weather_1" style={{'fontSize': '6rem'}}> </i>
-                        <span style={{'fontSize': '6rem', 'letterSpacing': '-0.4rem'}} id="val_tem">{data['实时气温'] && data['实时气温'].replace('℃', '°')}</span>
-                        <br/>
-                        <span style={{'fontSize': '1.1rem'}} id="val_time">{data['发布时间']}</span>
+            <div style={{width: '50vw', position: 'absolute', right: 0, top:0, padding: '15px'}}>
+                <div id="weather_block" style={{'transition': 'opacity 1s'}}>
+                    <table className="layui-table" style={{'backgroundColor': 'rgba(0,0,0,0)'}}>
+                        <tbody>
+                        <tr>
+                            <td><i className="iconfont table-icon icon-kongqishidu"> </i></td>
+                            <td>[<span id="val_hud" className="table-value">{data['湿度']}</span>]</td>
+                            <td><i className="iconfont table-icon icon-richu"> </i></td>
+                            <td>[<span id="val_sun_on" className="table-value">{data['日出时间']}</span>]</td>
+                            <td><i className="iconfont table-icon icon-riluo"> </i></td>
+                            <td>[<span id="val_sun_off" className="table-value">{data['日落时间']}</span>]</td>
+                            <td><i className="iconfont table-icon icon-daqiyali"> </i></td>
+                            <td>[<span id="val_press" className="table-value">{data['气压']}</span>]</td>
+                            <td><i className="iconfont table-icon icon-ziwaixianzhishu"> </i></td>
+                            <td>[<span id="val_uv" className="table-value">{data['紫外线']}</span>]</td>
+                            <td><i className="wind iconfont table-icon icon-wind-1"> </i></td>
+                            <td>[<span id="val_wind" className="table-value">{data['风级']}</span>]</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div style={{position: 'relative'}}>
+                        <div style={{
+                            'width': '17.5rem',
+                            'position': 'absolute',
+                            'padding': '0.875rem',
+                            'left': 0,
+                            'top': 0
+                        }}>
+                            <i className={`iconfont weather_1 ${weather_list[weather[0]]}`}
+                               style={{'fontSize': '10rem'}}> </i>
+                            {weather.length === 2 ? (
+                                <>
+                                <span className="weather_or" style={{'fontSize': '2rem'}}> </span>
+                                <i className="iconfont weather_2" style={{'fontSize': '5rem'}}> </i>
+                                </>
+                            ) : ''}
+                        </div>
+                        <div >
+                            <span style={{'fontSize': '1.1rem'}} id="val_weather">{data['天气']}</span>
+                            <span style={{'fontSize': '1.1rem'}} id="val_tem_r">{data['气温']}</span>
+                            <br/>
+                            <i className="iconfont weather_1" style={{'fontSize': '6rem'}}> </i>
+                            <span style={{'fontSize': '6rem', 'letterSpacing': '-0.4rem'}}
+                                  id="val_tem">{data['实时气温'] && data['实时气温'].replace('℃', '°')}</span>
+                            <br/>
+                            <span style={{'fontSize': '1.1rem'}} id="val_time">{data['发布时间']}</span>
+                        </div>
                     </div>
                 </div>
             </div>
-            </>
         )
     }
 }
