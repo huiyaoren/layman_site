ec6872d587fad7b61ba1a0945f7fa58ea8821237
hanlin <wslshanlin@sina.com>
2020-07-05 16:27:59 +0800

    fix: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 python3 -m pip install pip --upgrade -i https://pypi.douban.com/simple
-sudo python3 -m pip install -r requirements.txt -i https://pypi.douban.com/simple
+sudo python3 -m pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
 python3 manage.py
